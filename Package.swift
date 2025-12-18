fa667ed2cc1e0633cff131c41746c59088dd9370
Morgan Pretty <morgan.t.pretty@gmail.com>
2025-12-19 08:38:06 +1100

    libSession Swift Package Manager 1.5.9

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SessionUtil",
-            url: "https://github.com/session-foundation/libsession-util-spm/releases/download/1.5.8/libsession-util.xcframework.zip",
-            checksum: "b800407e72268166f1d35e1db3ec31a901cbcd66951468781b9ae1e6a2e24ffe"
+            url: "https://github.com/session-foundation/libsession-util-spm/releases/download/1.5.9/libsession-util.xcframework.zip",
+            checksum: "7d18dcd0f08628837237cb1e536574c3f51b472a94b86e011da034217085bcbe"
         )
     ]
 )
